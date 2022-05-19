e8eb1ea30f5faa4d0d8b3905acd1abde32cc50f5
xiaoduan.KD <1961018000@qq.com>
2022-05-19 14:08:01 +0800

    ✨ feat(二叉树): 前序遍历
    669.修剪二叉搜索树

---
+++ 669.修剪二叉搜索树.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=669 lang=javascript
+ *
+ * [669] 修剪二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {TreeNode}
+ */
+var trimBST = function (root, low, high) {
+  if (root === null) return null;
+  if (root.val < low) {
+    return trimBST(root.right, low, high);
+  }
+  if (root.val > high) {
+    return trimBST(root.left, low, high);
+  }
+  root.left = trimBST(root.left, low, high);
+  root.right = trimBST(root.right, low, high);
+  return root;
+};
+// @lc code=end
