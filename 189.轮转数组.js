9949bb18ff453b28d00e8b4c55b61128ccb0997f
xiaoduan.KD <1961018000@qq.com>
2022-05-19 16:50:09 +0800

    ✨ feat(数组): 189.轮转数组

---
+++ 189.轮转数组.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  if (nums.length === 1) return nums;
+  k = k % nums.length;
+  let spliceStart = nums.length - k;
+  let kNums = nums.splice(spliceStart);
+  if (spliceStart < 0) {
+    reverse(nums, 0, nums.length - 1);
+  }
+  nums.unshift(...kNums);
+};
+const reverse = (arr, start, end) => {
+  while (start < end) {
+    [arr[start++], arr[end--]] = [end, start];
+  }
+};
+// @lc code=end
