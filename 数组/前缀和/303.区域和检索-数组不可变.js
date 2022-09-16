3ad1c7dbdb4f8d06800f52c79cb39bdaea17e5ba
coderduan <1961018000@qq.com>
2022-09-16 10:52:11 +0800

    ✨ feat(前缀和): 303.区域和检索-数组不可变

---
+++ 数组/前缀和/303.区域和检索-数组不可变.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+
+/**
+ * @param {number[]} nums
+ */
+/**
+ * 时间复杂度 O(1)
+ * 空间复杂度 O(N)
+ * 思路
+ * - 前缀和 解法
+ * - 初始化是计算前缀和preSum,preSum第一位存储0，方便后面计算，所以长度要比nums+1
+ * - nums:     0   1   2   3    4    5
+ * -        【 3   5   2   -2   4    1 】
+ * - preSum:0  1   2   3   4    5    6
+ * -     【 0  3   8   10  8    12   13 】
+ * - 核心思路是我们 new 一个新的数组 preSum 出来，preSum[i] 记录 nums[0..i-1] 的累加和,看图 10 = 3 + 5 + 2：
+ * - 如果我想求索引区间 [1, 4] 内的所有元素之和，就可以通过 preSum[5] - preSum[1] 得出
+ * ![](https://coderduan-image.oss-cn-hangzhou.aliyuncs.com/img/202209161038322.png)
+ */
+var NumArray = function (nums) {
+  this.preSum = new Array(nums.length + 1).fill(0);
+  for (let i = 0; i < this.preSum.length; i++) {
+    this.preSum[i] = this.preSum[i - 1] + nums[i - 1];
+  }
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  return this.preSum[left + 1] + this.preSum[right];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
