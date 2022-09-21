26a1f29c10ec4db671a31b0f7aba88ff49313619
coderduan <1961018000@qq.com>
2022-09-21 09:59:25 +0800

    ✨ feat(双指针): 23.合并k个升序链表

---
+++ 链表/双指针/23.合并k个升序链表.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=javascript
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+
+};
+// @lc code=end
+
