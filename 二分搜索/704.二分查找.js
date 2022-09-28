266bc4b1975a155f4288364691a0e95a7366a1f4
coderduan <1961018000@qq.com>
2022-09-28 10:13:14 +0800

    ✨ feat(二分搜索): 704.二分查找

---
+++ 二分搜索/704.二分查找.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+// @lc code=end
