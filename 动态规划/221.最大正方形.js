c0697d9ad6651dae295b0f835405c37eb1bd7cde
coderduan <1961018000@qq.com>
2022-10-13 11:31:45 +0800

    ✨ feat(动态规划): 221.最大正方形

---
+++ 动态规划/221.最大正方形.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=221 lang=javascript
+ *
+ * [221] 最大正方形
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+  let maxSide = 0;
+  if (matrix === null || matrix.length === 0 || matrix[0].length === 0)
+    return maxSide;
+
+  const m = matrix.length,
+    n = matrix[0].length;
+  const dp = new Array(m).fill().map((_) => new Array(n).fill(0));
+
+  // 从1开始
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === "1") {
+        // 当遇到1时,根据左方，上方和左上方推到出当前边
+        if (i === 0 || j === 0) {
+          dp[i][j] = 1; // 当i||j为0 时，边长只能为1
+        } else {
+          // 木桶效应，取最小的边长+1
+          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        }
+      }
+      // 更新最大边长
+      maxSide = Math.max(maxSide, dp[i][j]);
+    }
+  }
+  return maxSide * maxSide;
+};
+// @lc code=end
