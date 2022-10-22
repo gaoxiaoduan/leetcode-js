a665b460aad3b804e1496c21f0c0f3fe953af932
coderduan <1961018000@qq.com>
2022-10-22 15:47:35 +0800

    ✨ feat(剑指Offer): 剑指 Offer 07. 重建二叉树

---
+++ 剑指Offer/剑指 Offer 07. 重建二叉树.js
@@ -0,0 +1,32 @@
+// 剑指 Offer 07. 重建二叉树: https://leetcode.cn/problems/zhong-jian-er-cha-shu-lcof/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  // build
+  // 寻找根节点和左右子树的根节点
+  const build = (preStart, preEnd, inStart, inEnd) => {
+    if (preStart > preEnd) return null;
+    const rootVal = preorder[preStart]; // 前序遍历的第一个节点是根结点
+    const index = inorder.indexOf(rootVal); // 在中序遍历中找到根节点的index，根节点左边是左子树，右边是右子树
+    const leftSize = index - inStart; // 在中序遍历中 根节点 - 开始 = leftSize
+
+    const root = new TreeNode(rootVal);
+    // 左子树，前序索引 （preStart + 1，preStart + leftSize） 中序索引（inStart,rootIndex -1）
+    root.left = build(preStart + 1, preStart + leftSize, inStart, index - 1);
+    // 右子树 前序（preStart + leftSize + 1, preEnd） 中序(rootIndex+1,inEnd)
+    root.right = build(preStart + leftSize + 1, preEnd, index + 1, inEnd);
+    return root;
+  };
+
+  return build(0, preorder.length - 1, 0, inorder.length - 1);
+};
