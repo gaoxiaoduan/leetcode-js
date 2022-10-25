382e3cd7d5813f1f4cd56b3218a62bfab816b515
coderduan <1961018000@qq.com>
2022-10-25 17:39:39 +0800

    ✨ feat(二叉树): 110.平衡二叉树

---
+++ 二叉树/110.平衡二叉树.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+//  时间复杂度 O(N)
+//  空间复杂度 O(1)
+
+//  思路
+//  - 为了判断是否平衡，拍脑袋的想法是计算每个节点的最大高度，但是每个节点都算一遍最大深度，时间复杂度比较高
+//  - 所以，最好过来想，只计算一次最大深度，在计算过程中判断二叉树是否平衡
+//  - 对于每个节点，先算出来左右子树的最大深度，然后在后序遍历的位置，然后根据 左右子树的最大高度 判断平衡性
+var isBalanced = function (root) {
+  let res = true;
+
+  const traverse = (root) => {
+    if (root === null) return 0;
+    if (!res) return; // res 已经为false了，可以停止遍历了
+
+    const leftMaxDepth = traverse(root.left);
+    const rightMaxDepth = traverse(root.right);
+    if (Math.abs(leftMaxDepth - rightMaxDepth) > 1) {
+      res = false;
+    }
+    return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+  };
+
+  traverse(root);
+  return res;
+};
+// @lc code=end
