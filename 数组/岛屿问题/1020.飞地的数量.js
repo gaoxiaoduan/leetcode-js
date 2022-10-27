25f1731cabf707b58f824586a9781439a74bb597
coderduan <1961018000@qq.com>
2022-10-27 15:21:43 +0800

    ✨ feat(岛屿问题): 1020.飞地的数量

---
+++ 数组/岛屿问题/1020.飞地的数量.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=1020 lang=javascript
+ *
+ * [1020] 飞地的数量
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+//  跟【1254.统计封闭岛屿的数目】类似都要先把边上的给淹了
+//  1254求的是封闭岛屿的数量，所以找到岛屿后需要再淹一下
+//  这个只需要求剩下岛屿的面积（数量）
+var numEnclaves = function (grid) {
+  const m = grid.length,
+    n = grid[0].length;
+
+  // 从 (i, j) 开始，将与之相邻的陆地都变成海水
+  const dfs = (i, j) => {
+    if (i < 0 || i >= m || j < 0 || j >= n) return;
+    if (grid[i][j] === 0) return; // 已经是水了
+
+    grid[i][j] = 0; // (i,j) 变成水
+    // 相邻的都变成水
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  };
+
+  for (let i = 0; i < m; i++) {
+    dfs(i, 0); // 把左边淹了
+    dfs(i, n - 1); // 把右边淹了
+  }
+
+  for (let j = 0; j < n; j++) {
+    dfs(0, j); // 把上边淹了
+    dfs(m - 1, j); // 把下边淹了
+  }
+
+  let res = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      // 如果发现岛屿
+      if (grid[i][j] === 1) {
+        res++;
+        // dfs(i, j); // 与【1254.统计封闭岛屿的数目】不同之处
+      }
+    }
+  }
+  return res;
+};
+// @lc code=end
