d07f87cd2e19181caa63451094ba415c43ee22a2
coderduan <1961018000@qq.com>
2022-11-04 11:03:12 +0800

    refactor(动态规划): 72.编辑距离

--- 动态规划/子序列类型/72.编辑距离.js
+++ 动态规划/子序列类型/72.编辑距离.js
@@ -52,4 +52,36 @@ var minDistance = function (word1, word2) {
 
   return dp(len1 - 1, len2 - 1);
 };
+
+// 自底向上-dp数组
+var minDistance1 = function (word1, word2) {
+  const m = word1.length, n = word2.length;
+  // dp[i][j]存储s1[0..i-1]->s2[0..j-1]的最少次数
+  const dp = new Array(m + 1).fill().map(_ => new Array(n + 1).fill(0));
+  // base case 
+  // i|j 走到头了，返回的就是剩下（i｜j）的数量
+  for (let i = 1; i <= m; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 1; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        // 跳过
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(
+          dp[i][j - 1] + 1, // 插入
+          dp[i - 1][j] + 1, // 删除
+          dp[i - 1][j - 1] + 1, // 替换
+        )
+      }
+    }
+  }
+
+  return dp[m][n];
+};
 // @lc code=end
