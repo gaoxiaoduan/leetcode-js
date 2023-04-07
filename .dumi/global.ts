bc3466fce56ebc874a8866395082379948385d27
coderduan <1961018000@qq.com>
2023-04-07 16:28:35 +0800

    feat(.dumi): 支持java&rust语言高亮

---
+++ .dumi/global.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import Prism from 'prism-react-renderer/prism';
+
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+
+require('prismjs/components/prism-java');
+require('prismjs/components/prism-rust');
